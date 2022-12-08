0b1252457222fef29a0e8c9dff032ebaa686b370
Eddy1112 <eddyreca@hotmail.com>
2022-12-07 20:46:49 -0600

    calculadora

---
+++ JAVA/operaciones.js
@@ -0,0 +1,26 @@
+function init(){
+   //variables
+   var result = document.getElementById("result");
+   var raiz = document.getElementById("result");
+   var reset = document.getElementById("result");
+   var borrar = document.getElementById("result");
+   var uno = document.getElementById("result");
+   var dos = document.getElementById("result");
+   var tres = document.getElementById("result");
+   var cuatro = document.getElementById("result");
+   var cinco = document.getElementById("result");
+   var seis = document.getElementById("result");
+   var siete = document.getElementById("result");
+   var ocho = document.getElementById("result");
+   var nueve = document.getElementById("result");
+   var cero = document.getElementById("result");
+   var suma = document.getElementById("result");
+   var  resta= document.getElementById("result");
+   var multiplicacion = document.getElementById("result");
+   var division = document.getElementById("result");
+   var masmenos = document.getElementById("result");
+   var raiz = document.getElementById("result");
+   var unosobrex = document.getElementById("result");
+   var punto = document.getElementById("result");
+   var igual = document.getElementById("result");
+}
